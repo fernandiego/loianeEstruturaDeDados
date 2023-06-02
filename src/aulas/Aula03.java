0ac7c3bc33e8d1edaaab33e03080c5171ca3d44a
fox <fernandodiego.sn@gmail.com>
2023-06-02 15:56:34 -0300

    configurando a idea, casca de banana

--- src/aulas/Aula03.java
+++ src/aulas/Aula03.java
@@ -1,5 +1,7 @@
+package aulas;
 public class Aula03 {
-    public static void main(String[] args){
-    System.out.println("teste");
+    public static void main(String[] args) {
+
+        System.out.println("teste");
     }
 }
