bf793fb19b99fa174f7e09d389124eaca7f2095a
fox <fernandodiego.sn@gmail.com>
2023-06-02 16:30:38 -0300

    aula 03 vetores loiane

--- src/aulas/Aula03.java
+++ src/aulas/Aula03.java
@@ -1,7 +1,20 @@
 package aulas;
+
+import vetor.Vetor;
 public class Aula03 {
     public static void main(String[] args) {
 
         System.out.println("teste");
-    }
+
+        Vetor vetor = new Vetor(3);
+
+        try {
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        }
 }
--- src/vetor/Vetor.java
+++ src/vetor/Vetor.java
@@ -1,14 +1,30 @@
+package vetor;
+
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        tamanho = 0;
     }
-    public void adiciona(String elemento) {
-        for (int i = 0; i < this.elementos.length; i++) {
-            if (this.elementos[i] == null) {
-                this.elementos[i] = elemento;
-            }
+
+    //    public void adiciona(String elemento) {
+//        for (int i = 0; i < this.elementos.length; i++) {
+//            if (this.elementos[i] == null) {
+//                this.elementos[i] = elemento;
+//                break;
+//            }
+//        }
+//    }
+    public void adiciona(String elemento) throws Exception {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
         }
+        else{
+            throw new Exception("Vetor está cheio, não foi possível adicionar elemento");
+        }
+
     }
 }
