74af7b8e9b5c9182614bb04d01f626922211b817
fox <fernandodiego.sn@gmail.com>
2023-06-06 17:45:51 -0300

    Aula 17, desempilhando elemento

---
+++ src/aulas/Aula17.java
@@ -0,0 +1,21 @@
+package aulas;
+
+import pilha.Pilha;
+
+public class Aula17 {
+    public static void main(String[] args) {
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        pilha.empilha(1);
+        pilha.empilha(2);
+        pilha.empilha(3);
+        pilha.empilha(4);
+
+        System.out.println(pilha);
+
+        System.out.println("Desempilhado o elemento " + pilha.desempilha());
+
+        System.out.println(pilha);
+    }
+
+}
