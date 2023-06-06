337bb83cfd85b7c1bf34c1cfc71aea3aab45f2c1
fox <fernandodiego.sn@gmail.com>
2023-06-06 16:15:09 -0300

    Aula 13, pilha. Muitos métodos aproveitados dos vetores

--- src/aulas/Aula13.java
+++ src/aulas/Aula13.java
@@ -1,7 +1,12 @@
 package aulas;
 
+import pilha.Pilha;
+
 public class Aula13 {
     public static void main(String[] args) {
 
+        Pilha<Integer> pilha = new Pilha<Integer>();
+        System.out.println(pilha);
+        System.out.println(pilha.tamanho());
     }
 }
