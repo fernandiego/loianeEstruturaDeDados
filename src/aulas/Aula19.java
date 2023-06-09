a1d4f6abaf3816e35459d632c636df7e8d023799
fox <fernandodiego.sn@gmail.com>
2023-06-09 14:46:07 -0300

    Iniciando o tema Filas, Aula 19 e 20

---
+++ src/aulas/Aula19.java
@@ -0,0 +1,13 @@
+package aulas;
+
+import fila.Fila;
+
+// Testando fila
+public class Aula19 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.estaVazia());
+        System.out.println(fila.tamanho());
+    }
+}
---
+++ src/fila/Fila.java
@@ -0,0 +1,17 @@
+package fila;
+
+import base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    public void enfileira(T elemento) {
+        this.adiciona(elemento);
+    }
+}
