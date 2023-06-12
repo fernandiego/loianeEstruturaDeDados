9029e08e5841ea9e095837823e2234a6a3c98571
fox <fernandodiego.sn@gmail.com>
2023-06-12 17:49:18 -0300

    Listas encadeadas, vídeo número 45 da série de estrutura de dados da Loiane

---
+++ src/lista/ListaEncadeadaTeste.java
@@ -0,0 +1,10 @@
+package lista;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
+}
